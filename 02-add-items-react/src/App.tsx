69ad261c507c391f6e3fcf229c83333968d4dfe8
renzodeveloper <rko2007hn@gmail.com>
2025-02-01 21:02:26 -0500

    CODING_WITH_REACT: Updating styles for the form

--- 02-add-items-react/src/App.tsx
+++ 02-add-items-react/src/App.tsx
@@ -1,27 +1,38 @@
 import React, { Component } from "react";
-import Menu from './Menu';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello, React 16!</h1>
-          <Menu />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <aside>
+          <h1>React Technical Test</h1>
+          <h2>Adding and Deletings items to the list</h2>
+
+          <form>
+            <label>
+              Items to add:
+              <input
+                name="item"
+                required
+                type="text"
+                placeholder="Videojuegos 🎮"
+              />
+            </label>
+            <button type="submit">Adding item to the list</button>
+          </form>
+        </aside>
+
+        <section>
+          <h2>List of items</h2>
+          <ul>
+            <li>Videogames 🎮</li>
+            <li>Books 📚</li>
+            <li>Series 📺</li>
+            <li>Movies 📽️</li>
+          </ul>
+        </section>
+      </main>
     );
   }
 }
