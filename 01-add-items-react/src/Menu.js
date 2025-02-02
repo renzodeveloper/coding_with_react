9b9a1225dc73529fb42438e1ffebf7499d33085f
renzodeveloper <rko2007hn@gmail.com>
2025-02-01 19:30:06 -0500

    CODING_WITH_REACT: Adding first two projects

---
+++ 01-add-items-react/src/Menu.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+    render() {
+        return (
+            <div>
+              <h1>Menu</h1>
+              <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+              </ul>
+            </div>
+          );
+    }
+}
+
+export default Menu;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Live Coding
+
+Resolving exercises using React 15 and React 16 with Javascript and Typescript
