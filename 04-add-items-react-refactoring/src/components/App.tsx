c2dda48109d16eb2f06fc442d48721c2b9d1b71f
renzodeveloper <rko2007hn@gmail.com>
2025-02-09 00:13:25 -0500

    CODING_WITH_REACT: Organizing files for the deploy in Netlify

--- 04-add-items-react-refactoring/src/components/App.tsx
+++ 04-add-items-react-refactoring/src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { ItemComponent } from "./ItemComponent";
 import { useItems } from "../hooks/useItems";
