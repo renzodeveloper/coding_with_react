52a87aeb5e62f1fba4e1480e0afe1c8e563ad9c2
renzodeveloper <rko2007hn@gmail.com>
2025-02-19 23:28:28 -0500

    CODING_WITH_REACT: Basic React Technical Test refactoring with using two useEffect

--- 06-react-technical-test/src/App.jsx
+++ 06-react-technical-test/src/App.jsx
@@ -7,32 +7,49 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 export function App() {    
     const [fact, setFact] = useState();
     const [imageUrl, setImageUrl] = useState();
+    const [factError, setFactError] = useState();
 
+    // First effect with one responsability
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
-        .then(res => res.json())
+        .then(res => {
+            if (!res.ok) throw new Error('Error fetching fact');//Sending error response to the catch block
+            return res.json();
+        })
         .then(data => {
             const { fact } = data;
             setFact(fact);
-
-            const threeFirstWords = data.fact.split(' ').slice(0, 3).join(' ');
-
-            fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
-            .then(res => res.json())
-            .then(response => {
-                console.log(JSON.stringify(response));
-
-                const { url } = response;
-                setImageUrl(url);
-            })
+        })
+        .catch(error => {
+            // Receiving error response
+            // Getting error from the request like lost connectivity
+            setFactError(error.message);            
         })
     }, []);
+
+    //Second effect with other responsability
+    useEffect(() => {
+        if (!fact) return;
+
+        const threeFirstWords = fact.split(' ').slice(0, 3).join(' ');
+        console.log(threeFirstWords);
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
+        .then(res => res.json())
+        .then(response => {
+            console.log(JSON.stringify(response));
+
+            const { url } = response;
+            setImageUrl(url);
+        })
+    }, [fact]);
 
     return (
       <main>
         <h1>Kittens application</h1>
         <section>
           {fact && <p>{fact}</p>}
+          {factError && <p>{factError}</p>}
           {imageUrl && (
             <img
               src={imageUrl}
